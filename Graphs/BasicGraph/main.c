1f55598d410ba65611a673bdd27af662bce209b5
Mohamed Ashraf <68484397+mohamedashraf-eng@users.noreply.github.com>
2022-06-30 23:54:24 +0200

    Graph using adj list

--- Graphs/BasicGraph/main.c
+++ Graphs/BasicGraph/main.c
@@ -1,1 +1,31 @@
-x
+//=============================> .INC
+#include "MyGraphLib.h"
+//=============================> .DATA
+
+//=============================> .MAIN
+int main(void)
+{
+    // Change terimnal color: GREEN.
+    system("COLOR 0A");
+
+    GraphEdge myedges[] =
+    {
+        {1,30 ,5},
+        {9,4, 7},
+        {4,2, 1},
+        {1,4, 3},
+        {9,1, 3},
+        {1,2, 4}
+    };
+ 
+    uint16_t n = sizeof(myedges) / sizeof(myedges[0]);
+
+    MyGraph *mygraph = CreateGraph(myedges, n);
+
+    printgraph(mygraph);
+
+    printf("\n");
+    return 0;
+}
+
+//=============================> .FUNC
